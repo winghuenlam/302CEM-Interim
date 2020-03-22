1fbdb7166756f4593c585e0fa279f9e965a585d2
Maddie Lam <maddie.lam@veztan.com>
2020-03-22 19:26:45 +0800

    search function

--- department_store_server.js
+++ department_store_server.js
@@ -27,6 +27,14 @@ const ds_db = mysql.createConnection({
 
 ds_db.connect();
 
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+app.get('/ds/upload', function(req,res){
+    res.sendFile(path.join(__dirname + '/ds_upload.html'));
+})
+
 app.post('/ds/input-order', upload.single('csvfile'), function(req,res){
     // console.log(req.body)
     var csvfile = req.file
@@ -97,6 +105,12 @@ app.post('/ds/input-order', upload.single('csvfile'), function(req,res){
                     }
                     }).then((response)=> {
                         if (response['data']['success']==true){
+                            var updateIdSql = `UPDATE orders SET lc_delivery_id = ${response['data']['delivery_id']} 
+                                                WHERE id=${json['Order']['order_no']}`
+                            ds_db.query(updateIdSql, (err, result)=>{
+                                if(err) throw err;
+                            })
+
                             var totalPrice = 0
                             var itemsTableHtml = ''
                             json['Order']['order_item'].map((v,i) => {
@@ -137,9 +151,6 @@ app.post('/ds/input-order', upload.single('csvfile'), function(req,res){
                         }else{
                             var html = `Unsuccessful.`
                         }
-
-                        
-
                         res.set('Content-Type', 'text/html');
                         res.send(new Buffer(html));
 
@@ -157,8 +168,64 @@ app.post('/ds/input-order', upload.single('csvfile'), function(req,res){
 });
 
 
-app.get('/ds/upload', function(req,res){
-    res.sendFile(path.join(__dirname + '/ds_upload.html'));
+app.get('/ds/search', function(req,res){
+    res.sendFile(path.join(__dirname + '/ds_search.html'));
+})
+
+app.get('/ds/search/submit', function(req,res){
+    var orderId = req.query.orderid
+    console.log(orderId)
+    var infoSql = `SELECT c.id AS c_id, c.name AS c_name, o.id AS o_id, o.order_date, o.lc_delivery_id FROM customer c, orders o
+                    where c.id = o.customer_id and o.id = ${orderId};`
+    var itemSql = `SELECT p.id AS code, p.name, o.quantity, o.price FROM order_product o, product p
+                    where o.product_id=p.id and o.order_id = ${orderId};`
+    ds_db.query(infoSql, (err, c_res)=>{
+        if(err) throw err;
+        var c_result = c_res[0]
+        if(c_result){
+            ds_db.query(itemSql, (err, i_res)=>{
+            if(err) throw err;
+            var itemsTableHtml = ''
+            var totalPrice = 0
+            i_res.map((v,i) => {
+                totalPrice += v['price']
+                itemsTableHtml += 
+                    `<tr>
+                        <td>${v['name']}</td>
+                        <td style="text-align:center">${v['quantity']}</td>
+                        <td style="text-align:center">${v['price']}</td>
+                    </tr>`
+            })
+
+            var html = `
+            <h2>Order ID: ${orderId}</h2>
+            <p>Delivery ID: ${c_result['lc_delivery_id']}</p>
+            <p>Customer: ${c_result['c_name']} (ID: ${c_result['c_id']})</p>
+            <p>Order date: ${c_result['order_date']}</p> <br/>
+            <h4>Ordered Items</h4>
+            <table>
+                <tr>
+                    <th style="width:200px; text-align:left;">Item</th>
+                    <th style="width:100px; text-align:center">Quantity</th>
+                    <th style="width:100px; text-align:center">Price</th>
+                </tr>
+                ${itemsTableHtml}
+                <tr>
+                    <th></th>
+                    <th style="text-align:right">Total Price</th>
+                    <th>${totalPrice}</th>
+                </tr>
+            </table>
+            `
+        
+            res.set('Content-Type', 'text/html');
+            res.send(new Buffer(html));
+            })
+        }else{
+            res.send('No Record');
+        }
+
+    })
 })
 
 app.listen(3000, ()=>{
--- logistics_company_server.js
+++ logistics_company_server.js
@@ -5,8 +5,8 @@ const app = express();
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// const fs = require('fs')
-// var parse = require('csv-parse');
+var path = require('path');
+app.use(bodyParser.urlencoded());
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -19,6 +19,10 @@ const lc_db = mysql.createConnection({
 });
 
 lc_db.connect();
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
 
 app.post('/lc/input-order', function(req,res){
     var recieveOrder = new Promise((resolve, reject) => {
@@ -53,6 +57,60 @@ app.post('/lc/input-order', function(req,res){
     })
     
 });
+
+app.get('/lc/search', function(req,res){
+    res.sendFile(path.join(__dirname + '/lc_search.html'));
+})
+
+app.get('/lc/search/submit', function(req,res){
+    var deliveryId = req.query.deliveryid
+    var infoSql = `SELECT * FROM delivery WHERE id = ${deliveryId}`
+    var itemSql = `SELECT * FROM item WHERE delivery_id = ${deliveryId}`
+    lc_db.query(infoSql, (err, info_res)=>{
+        if(err) throw err;
+        var info_result = info_res[0]
+        if(info_result){
+            lc_db.query(itemSql, (err, item_res)=>{
+                console.log(item_res)
+                if(err) throw err;
+                var itemsTableHtml = ''
+                item_res.map((v,i) => {
+                    itemsTableHtml += 
+                        `<tr>
+                            <td style="text-align:center">${v['ds_product_id']}</td>
+                            <td style="text-align:center">${v['quantity']}</td>
+                            <td style="text-align:center">${v['delivery_status']}</td>
+                        </tr>`
+                })
+
+                var html = `
+                <h2>Delivery ID: ${deliveryId}</h2>
+                <p>Order ID: ${info_result['ds_order_id']}</p>
+                <p>Customer ID: ${info_result['ds_customer_id']}</p>
+                <p>Payment Amount: ${info_result['payment_amount']}</p>
+                <p>Cash on delivery: ${info_result['cash_on_delivery']}</p>
+                <p>Created date: ${info_result['created_date']}</p> <br/>
+                <h3>Delivery Status</h3>
+                <table>
+                    <tr>
+                        <th style="width:100px; text-align:center;">Item ID</th>
+                        <th style="width:100px; text-align:center">Quantity</th>
+                        <th style="width:100px; text-align:center">Status</th>
+                    </tr>
+                    ${itemsTableHtml}
+                </table>
+                `
+            
+                res.set('Content-Type', 'text/html');
+                res.send(new Buffer(html));
+            })
+        }else{
+            res.send('No Record');
+        }
+
+    })
+
+})
 
 
 app.listen(4000, ()=>{
